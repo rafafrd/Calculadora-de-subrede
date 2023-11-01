e13c3d281b7bc40e0ea1f4e587f3fbfd7e299e8b
Caio Franson <caio.franson@gmail.com>
2023-10-31 23:20:09 -0300

    adicionando novas funções javascript

--- src/script/main.js
+++ src/script/main.js
@@ -1,16 +1,41 @@
-// funcao para validar se o endereço ip digitado é valido
-function validarIp(enderecoIp) {
-    var errorDiv = document.getElementById("mensagemErro");
-
-    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(enderecoIp)) {
-        errorDiv.style.display = "none"; // Esconde a mensagem de erro se o IP for válido
-        errorDiv.classList.remove("shake"); // Remove a classe "shake"
-        return true;
+// Função para exibir mensagem de erro e aplicar classe "shake"
+function exibirErro(divErro, valido) {
+    if (valido == true) {
+        divErro.style.display = "none";
+        divErro.classList.remove("shake");
+    } else {
+        divErro.style.display = "block";
+        divErro.classList.add("shake");
     }
-    
-    // Exibe a mensagem de erro e aplica a classe "shake"
-    errorDiv.style.display = "block";
-    errorDiv.classList.add("shake");
+}
 
-    return false;
+function validarIp(enderecoIp) {
+    var divErro = document.getElementById("mensagemErro");
+    var valido = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(enderecoIp);
+    exibirErro(divErro, valido);
+    return valido;
+}
+
+function validarMascara(mascara) {
+    var divErro = document.getElementById("mensagemErroMascara");
+    var valido = /^(255)\.(0|128|192|224|240|248|252|254|255)\.(0|128|192|224|240|248|252|254|255)\.(0|128|192|224|240|248|252|254|255)$/.test(mascara);
+    
+    if (valido == true) {
+        var pedacos = mascara.split(".");
+        var binario = pedacos.map(function (octeto) {
+            return parseInt(octeto).toString(2).padStart(8, "0");
+        }).join(".");
+
+        valido = /^1*0*1*$/.test(binario.replace(/\./g, ""));
+    }
+
+    exibirErro(divErro, valido);
+    return valido;
+}
+
+function validarCidr(cidr) {
+    var divErro = document.getElementById("mensagemErroCidr");
+    var valido = cidr >= 1 && cidr <= 30;
+    exibirErro(divErro, valido);
+    return valido;
 }
