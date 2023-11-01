7ad2947d9c2a89630850015bb6135601710145e5
Caio Franson <caio.franson@gmail.com>
2023-10-31 23:25:00 -0300

    changing classes.php and adding styles

--- src/php/classes.php
+++ src/php/classes.php
@@ -1,52 +1,88 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Descobrir Classe de IP</title>
-    <link rel="stylesheet" href="css/main.css">
-    <script src="js/main.js"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Classe IP</title>
+  <link rel="stylesheet" href="../../public/style/style.css">
+  <link rel="stylesheet" href="../../public/style/header.css">
+  <link rel="stylesheet" href="../../public/style/footer.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1>Descobrir Classe de IP</h1>
+  <header>
+    <nav class="nav">
+      <img src="../../public/img/logo.png" class="logo" alt="Logo Nerdfix" />
+      <div>
+        <input type="checkbox" id="menu" >
+        <label for="menu" class="label-menu">
+          <img src="../../public/img/menu.svg" alt="Menu" class="menu-img" />
+        </label>
+        <ul class="nav-items">
+          <a href="/src/pages/index.html" class="nav-item">Início</a>
+          <a href="/src/pages/classe.html" class="nav-item">Descobrir Classe do IP</a>
+          <a href="/src/pages/cidr.html" class="nav-item">Converter CIDR</a>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <main>
+
     <form method="POST" action="" onsubmit="return validarIp(document.getElementById('ip').value);">
-        <label for="ip">Digite o endereço IP:</label>
-        <input type="text" id="ip" name="ip" placeholder="Exemplo: 192.168.1.1">
-        <input type="submit" value="Descobrir">
+      <h1 class="title">Descobrir a Classe do IP</h1>
+      <div class="input-container">
+        <label for="ip" class="label">Digite o endereço IP:</label>
+        <input type="text" id="ip" name="ip" placeholder="Exemplo: 192.168.1.1" class="input-ip">
+      </div>
+      <input type="submit" value="Descobrir" class="button">
     </form>
 
-    <div id="message-error" class="message-error">Endereço IP inválido!</div>
+    <div class="result" id="result">
 
-<?php
+      <!-- TODO: DESCOBRIR PQ O ERRO NAO TA APARECENDO -->
+      <!-- QUALQUER COISA COLOCAR SÓ UM ALERT MESMO AO INVES DE PISCAR O ERRO -->
+      <div id="mensagemErroIp" class="mensagemErro">Endereço IP inválido!</div>
+    
+      <!-- TODO: ESTILIZAR O RESULTADO, DEIXAR MAIOR, E CENTRALIZAR NA ALTURA -->
+      <?php
+          if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+              // Verifique se o formulário foi enviado
+              if (isset($_POST["ip"])) {
 
-        // Verifique se o formulário foi enviado
-        if (isset($_POST["ip"])) {
+                  # ip digitado pelo usuario
+                  $ip = $_POST["ip"];
 
-            # ip digitado pelo usuario
-            $ip = $_POST["ip"];
+                  # usando o explode para separar os octetos
+                  $octetos = explode('.', $ip);
 
-            # usando o explode para separar os octetos
-            $octetos = explode('.', $ip);
+                  # separando o primeiro octeto e convertendo em numeros inteiros
+                  $primeiroOcteto = intval($octetos[0]);
 
-            # separando o primeiro octeto e convertendo em numeros inteiros
-            $primeiroOcteto = intval($octetos[0]);
+                  # conferindo os ranges para descobrir a classe
+                  if ($primeiroOcteto > 0 && $primeiroOcteto < 128) {
+                      echo "IP classe A";
+                  } elseif ($primeiroOcteto > 127 && $primeiroOcteto < 192) {
+                      echo "IP classe B";
+                  } elseif ($primeiroOcteto > 191 && $primeiroOcteto < 224) {
+                      echo "IP classe C";
+                  } elseif ($primeiroOcteto > 223 && $primeiroOcteto < 240) {
+                      echo "Range de Multicast";
+                  } elseif ($primeiroOcteto > 239 && $primeiroOcteto < 256) {
+                      echo "Range reservado para uso futuro";
+                  }
 
-            # conferindo os ranges para descobrir a classe
-            if ($primeiroOcteto > 0 && $primeiroOcteto < 128) {
-                echo "IP classe A";
-            } elseif ($primeiroOcteto > 127 && $primeiroOcteto < 192) {
-                echo "IP classe B";
-            } elseif ($primeiroOcteto > 191 && $primeiroOcteto < 224) {
-                echo "IP classe C";
-            } elseif ($primeiroOcteto > 223 && $primeiroOcteto < 240) {
-                echo "Range de Multicast";
-            } elseif ($primeiroOcteto > 239 && $primeiroOcteto < 256) {
-                echo "Range reservado para uso futuro";
-            }
+              }
+          }
+      ?>
+    </div>
 
-        }
-    }
-?>
-
+  </main>
+  <footer>
+    <img src="../../public/img/github-original.svg" alt="github" />
+    <p>Copyright © 2023 CFRANS - Rafafrd</p>
+  </footer>
 </body>
 </html>
